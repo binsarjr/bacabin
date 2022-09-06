5d3eeb1623f9a04b86134f1cd73c7aa411f7ae4b
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-09-06 07:49:00 +0700

    add chapter nav end of page

--- src/routes/[server]/read/[...chapter_link]/+page.svelte
+++ src/routes/[server]/read/[...chapter_link]/+page.svelte
@@ -28,3 +28,21 @@
 		<img data-src={images} data-waiting>
 	{/each}
 </div>
+
+<div class="flex gap-1 md:gap-5 justify-center items-center my-5">
+	<div>
+        {#if data.item.prev}
+		<a href="/{data.server}/read/{data.item.prev}" class="text-sm md:text-base py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black">Chapter Selanjutnya</a>
+        {/if}
+	</div>
+	<div>
+        {#if data.item.showLink}
+		<a href="/{data.server}/{data.item.showLink}" class="text-sm md:text-base py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black">Daftar Chapter</a>
+        {/if}
+	</div>
+    <div>
+        {#if data.item.next}
+		<a href="/{data.server}/read/{data.item.next}" class="text-sm md:text-base py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black">Chapter Selanjutnya</a>
+        {/if}
+	</div>
+</div>
