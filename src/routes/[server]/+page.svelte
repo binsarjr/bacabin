55567086905e4dae082101d657233548d41ce8bd
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-09-18 22:25:55 +0700

    chore: recreate element

--- src/routes/[server]/+page.svelte
+++ src/routes/[server]/+page.svelte
@@ -27,65 +27,80 @@
 			search();
 		}
 	}
-
 </script>
 
 <svelte:head>
 	<title>{data.server.name} - BacaBin</title>
-	<link rel="icon" href="{data.server.logo}" />
+	<link rel="icon" href={data.server.logo} />
 </svelte:head>
 
 <div class="content">
-
-<div class="my-10 mx-auto">
-	<img src={data.server.logo} alt={data.server.name} width="100px" />
-	<h5>
-		@credit: <a href={data.server.website} class="text-blue-500 hover:text-blue-800"
-			>{data.server.website}</a
-		>
-	</h5>
-	<div class="my-10 tracking-wide">
-		<p>
-			Selamat Datang di server <strong>{data.server.name}</strong>. Silakan menikmati waktumu
-		</p>
-	</div>
-	<label for="cari">
-		<span>Silakan Cari Komik Yang Anda inginkan</span>
-		<input autocomplete="false" type="text" class="form-control" id="cari" bind:value={q} on:keydown={keydown} minlength="2" required />
-		<span
-			>Silakan tekan [ENTER] atau tunggu 5 detik setelah Anda berhenti mengetik dan secara otomatis
-			akan melakukan pencarian</span
-		>
-	</label>
-	{#if q.length}
-		<div class="mt-5">
+	<div class="my-10 mx-auto">
+		<img src={data.server.logo} alt={data.server.name} width="100px" />
+		<h5>
+			@credit: <a href={data.server.website} class="text-blue-500 hover:text-blue-800"
+				>{data.server.website}</a
+			>
+		</h5>
+		<div class="my-10 tracking-wide">
 			<p>
-				Hasil Pencarian dari <strong>{q}</strong>.
+				Selamat Datang di server <strong>{data.server.name}</strong>. Silakan menikmati waktumu
 			</p>
 		</div>
-	{/if}
-</div>
-
-<div class="py-3">
-	<h2 class="mb-2">Baca Komik</h2>
-	<div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-5 gap-y-10">
-		{#if data.lists.length === 0}
-			<div>Data Tidak Ditemukan</div>
-		{/if}
-		{#each data.lists as list}
-			<div class="cardpost">
-				<a href={[currentPathname, list.show].join('/')}>
-					<div class="image">
-						<img  data-src={list.img} src="/loading.gif" class="lazyload rounded" loading="lazy" alt="[img] {list.img}" width="100%" />
-						<div class="text-image">{list.title}</div>
-					</div>
-					<div>
-						<p class="truncate ...">{list.title}</p>
-					</div>
-				</a>
+		<label for="cari">
+			<span>Silakan Cari Komik Yang Anda inginkan</span>
+			<input
+				autocomplete="false"
+				type="text"
+				class="form-control"
+				id="cari"
+				bind:value={q}
+				on:keydown={keydown}
+				minlength="2"
+				required
+			/>
+			<span
+				>Silakan tekan [ENTER] atau tunggu 5 detik setelah Anda berhenti mengetik dan secara
+				otomatis akan melakukan pencarian</span
+			>
+		</label>
+		{#if q.length}
+			<div class="mt-5">
+				<p>
+					Hasil Pencarian dari <strong>{q}</strong>.
+				</p>
 			</div>
-		{/each}
+		{/if}
 	</div>
-</div>
 
+	<div class="py-3">
+		<h2 class="mb-2">Baca Komik</h2>
+		<div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-5 gap-y-10">
+			{#if data.lists.length === 0}
+				<div>Data Tidak Ditemukan</div>
+			{/if}
+			{#key $page.url.toString()}
+				{#each data.lists as list}
+					<div class="cardpost">
+						<a href={[currentPathname, list.show].join('/')}>
+							<div class="image">
+								<img
+									data-src={list.img}
+									src="/loading.gif"
+									class="lazyload rounded"
+									loading="lazy"
+									alt="[img] {list.img}"
+									width="100%"
+								/>
+								<div class="text-image">{list.title}</div>
+							</div>
+							<div>
+								<p class="truncate ...">{list.title}</p>
+							</div>
+						</a>
+					</div>
+				{/each}
+			{/key}
+		</div>
+	</div>
 </div>
