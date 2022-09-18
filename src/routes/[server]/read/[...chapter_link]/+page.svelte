aad6617c47094c186348ff628f47846b35ff5e71
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-09-18 20:14:16 +0700

    fix: mounting once

--- src/routes/[server]/read/[...chapter_link]/+page.svelte
+++ src/routes/[server]/read/[...chapter_link]/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import 'lazysizes';
+	// import a plugin
+	import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 	import { afterNavigate, beforeNavigate, goto } from '$app/navigation';
 
 	import { page } from '$app/stores';
@@ -9,74 +12,68 @@
 	import GotoDown from '$lib/components/GotoDown.svelte';
 	import { historyChapter, historyKomik } from '$lib/stores/history';
 	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+	export let data: PageData;
 
-	let prev = $page.data.item.prev ? `/${$page.data.server}/read/${$page.data.item.prev}` : null;
-	let next = $page.data.item.next ? `/${$page.data.server}/read/${$page.data.item.next}` : null;
-	let chapterList = $page.data.item.showLink
-		? `/${$page.data.server}/${$page.data.item.showLink}`
-		: null;
-	let images = $page.data.item.chapterImages;
+	let prev = data.item.prev ? `/${data.server}/read/${data.item.prev}` : null;
+	let next = data.item.next ? `/${data.server}/read/${data.item.next}` : null;
+	let chapterList = data.item.showLink ? `/${data.server}/${data.item.showLink}` : null;
+	let images = data.item.chapterImages;
 	$: {
-		prev = $page.data.item.prev ? `/${$page.data.server}/read/${$page.data.item.prev}` : null;
-		next = $page.data.item.next ? `/${$page.data.server}/read/${$page.data.item.next}` : null;
-		chapterList = $page.data.item.showLink
-			? `/${$page.data.server}/${$page.data.item.showLink}`
-			: null;
-		images = $page.data.item.chapterImages;
+		prev = data.item.prev ? `/${data.server}/read/${data.item.prev}` : null;
+		next = data.item.next ? `/${data.server}/read/${data.item.next}` : null;
+		chapterList = data.item.showLink ? `/${data.server}/${data.item.showLink}` : null;
+		images = data.item.chapterImages;
 	}
 	async function save() {
+		const historyData = {
+			title: data.item.title,
+			server: data.server,
+			id: data.showLink,
+			link: $page.url.toString()
+		};
 		$historyKomik = [
-			{
-				title: $page.data.item.title,
-				server: $page.data.server,
-				id: $page.data.showLink,
-				link: $page.url.toString()
-			},
-			...$historyKomik.filter((history) => history.id != $page.data.showLink)
+			historyData,
+			...$historyKomik.filter((history) => history.id != data.showLink)
 		];
 		$historyChapter = [
-			{
-				title: $page.data.item.title,
-				server: $page.data.server,
-				id: $page.data.server + $page.data.item.title,
-				link: $page.url.toString()
-			},
-			...$historyChapter.filter(
-				(history) => history.id != $page.data.server + $page.data.item.title
-			)
+			historyData,
+			...$historyChapter.filter((history) => history.id != data.server + data.item.title)
 		];
 	}
 
 	async function reload() {
 		goto(window.location.href, { noscroll: true });
 	}
-	// onMount(() => {
-	// 	lozad().observe()
-	// })
 	save();
 </script>
 
 <svelte:head>
-	<title>{$page.data.item.title}</title>
+	<title>{data.item.title}</title>
 </svelte:head>
-<GotoDown />
-<BackToTop />
-<div class="content">
-	<div class="text-center">
-		<h1>{$page.data.item.title}</h1>
+
+<!-- Recreate element when value key is change -->
+{#key $page.url.toString()}
+	<GotoDown />
+	<BackToTop />
+	<div class="content">
+		<div class="text-center">
+			<h1>{data.item.title}</h1>
+		</div>
+		<ChapterPrevNext {prev} {next} {chapterList} />
 	</div>
-	<ChapterPrevNext {prev} {next} {chapterList} />
-</div>
-<div class="flex flex-col justify-center items-center">
-	{#each images as image}
-		{@const imageLink = image}
-		<img src="{imageLink}" loading="lazy" />
-	{/each}
-</div>
-<div id="reload" on:click={reload}>Reload</div>
-<div class="content mb-[30vh]">
-	<ChapterPrevNext {prev} {next} {chapterList} />
-</div>
+	<div class="flex flex-col justify-center items-center">
+		{#each images as image}
+			{@const imageLink = image}
+			<img data-src={imageLink} src="/loading.gif" class="lazyload" loading="lazy" />
+			<!-- <img src={imageLink} loading="lazy" /> -->
+		{/each}
+	</div>
+	<div id="reload" on:click={reload}>Reload</div>
+	<div class="content mb-[30vh]">
+		<ChapterPrevNext {prev} {next} {chapterList} />
+	</div>
+{/key}
 
 <style>
 	#reload {
