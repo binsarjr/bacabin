59f51b666123dd7beb742d436c87ce860aef957b
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-09-20 16:30:10 +0700

    refactor: concurrency image proccess

--- src/lib/browser-supports.ts
+++ src/lib/browser-supports.ts
@@ -16,20 +16,32 @@ export const imgLazyLoading = async () => {
     imgEvent.on('done', _ => {
         isDone = true
     })
-    document.querySelectorAll('img[data-src]').forEach((_) => {
+    const imgSelectors = 'img[data-waiting-src]'
+    document.querySelectorAll(imgSelectors).forEach((_) => {
         const el = _ as HTMLImageElement
         if (!el.src) el.src = '/loading.gif'
-        // Read More: http://afarkas.github.io/lazysizes
-        el.classList.add('lazyload')
         // Read More: https://web.dev/browser-level-image-lazy-loading/
-        el.setAttribute('loading','lazy')
+        el.setAttribute('loading', 'lazy')
     })
-    for (const _ of document.querySelectorAll('img[data-src][data-waiting]')) {
+
+    // Save concurrency progres
+    let futures = []
+    const totalImages = document.querySelectorAll(imgSelectors).length
+    // totalImage * 2%
+    const percentOfTotalImageNeeded = Math.floor(totalImages * 5 / 100)
+    const conccurrency = percentOfTotalImageNeeded > 0 ? percentOfTotalImageNeeded : 5;
+
+    for (const _ of document.querySelectorAll(imgSelectors)) {
         if (isDone) {
             return
         }
         const el: HTMLImageElement = _ as HTMLImageElement
-        el.src = el.dataset.src as string
-        await waitImgLoaded(el)
+        el.src = el.dataset.waitingSrc as string
+        if (futures.length < conccurrency) futures.push(waitImgLoaded(el))
+        else {
+            await Promise.all(futures)
+            futures = []
+        }
     }
+    if (futures.length) await Promise.all(futures)
 }
