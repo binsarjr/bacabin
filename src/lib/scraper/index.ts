7c6b9005bc54ad821e794ea299d4147bd9e4ca04
holiq <holiq.ibrahim376@gmail.com>
2022-10-01 01:42:26 +0700

    feat: add shield manga server

--- src/lib/scraper/index.ts
+++ src/lib/scraper/index.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 import type { IBaseKomik } from './BaseKomik/interfaces';
 import Manganato from './en/Manganato';
 import Mangastic from './en/Mangastic';
+import ShieldManga from './en/ShieldManga';
 import Bacakomik from './id/Bacakomik';
 import KomikIndo from './id/KomikIndo';
 import type { ServerList } from './types/Server';
@@ -11,19 +12,20 @@ const servers: { [i: ServerList]: IBaseKomik } = {
 	bacakomik: Bacakomik,
 	manganato: Manganato,
 	mangastic: Mangastic,
+	shieldmanga: ShieldManga
 };
 
 export const serverLists = () => {
-	return Object.keys(servers).map(key => {
-		const server = servers[key]
+	return Object.keys(servers).map((key) => {
+		const server = servers[key];
 		return {
 			title: server.name,
 			url: server.website,
 			img: server.logo,
 			server: key
-		}
-	})
-}
+		};
+	});
+};
 export const getServerByKeyOrFail = (key: string) => {
 	const server = servers[key];
 	if (!server) throw error(404);
