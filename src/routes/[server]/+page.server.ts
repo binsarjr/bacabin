1d28342a8f8fe97f13ed8ec29cb20d78f6268e0b
holiq <holiq.ibrahim376@gmail.com>
2022-10-02 15:11:18 +0700

    style: change let and var to const

--- src/routes/[server]/+page.server.ts
+++ src/routes/[server]/+page.server.ts
@@ -7,7 +7,7 @@ import type { ServerList } from '$lib/scraper/types/Server';
 import { error, type Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ params, url, parent, depends }) => {
-	let server = getServerByKeyOrFail(params.server as string);
+	const server = getServerByKeyOrFail(params.server as string);
 	const q = url.searchParams.get('q') || '';
 	try {
 		const lists = await server.list(url.searchParams);
