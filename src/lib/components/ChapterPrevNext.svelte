d894de1f9c81f13d4022a35129f62653453aff44
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-02-19 23:02:14 +0700

    ref: change prev next icon

--- src/lib/components/ChapterPrevNext.svelte
+++ src/lib/components/ChapterPrevNext.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
+	import ArrowLeft from '~icons/mdi/arrow-left-bold';
+	import ArrowRight from '~icons/mdi/arrow-right-bold';
 	export let prev: string | null;
 	export let chapterList: string | null;
 	export let next: string | null;
 </script>
 
-<div class="flex flex-col md:flex-row gap-10 my-5 md:justify-center md:items-center">
+<div class="flex flex-row gap-10 my-5 justify-end items-center">
 	<div>
+		{#if chapterList}
+			<a
+				href={chapterList}
+				class="py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black"
+				>Daftar Chapter</a
+			>
+		{/if}
+	</div>
+	<div class="flex gap-2">
 		{#if prev}
 			<a
 				href={prev}
-				class="py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black"
-				>Chapter Sebelumnya</a
+				class="py-2 px-8 inline-block border border-white rounded hover:bg-white hover:text-black"
+				><ArrowLeft /></a
 			>
 		{/if}
+		{#if next}
+			<a
+				href={next}
+				class="py-2 px-8 inline-block border border-white rounded hover:bg-white hover:text-black"
+				><ArrowRight /></a
+			>
+		{/if}
+	</div>
+	<!-- <div>
+		{#if prev}{/if}
 	</div>
 	<div class="self-center">
 		{#if chapterList}
@@ -24,12 +45,15 @@
 		{/if}
 	</div>
 	<div class="self-end">
-		{#if next}
+		{#if next}<a
+				href={prev}
+				class="py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black"
+				><ArrowLeft /></a
+			>
 			<a
 				href={next}
 				class="py-1 px-2 md:py-2 md:px-4 border border-white rounded hover:bg-white hover:text-black"
-				>Chapter Selanjutnya</a
-			>
+			/>
 		{/if}
-	</div>
+	</div> -->
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import Icons from 'unplugin-icons/vite'
+import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+			autoInstall: true
+		})
+	],
 	ssr: {
 		noExternal: ['devalue']
 	}
