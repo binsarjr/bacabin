77263752bd279d5f76b99fec03fdae514b124d9f
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-18 02:04:17 +0700

    implement query string too

--- src/routes/services/[server]/[...show]/+server.ts
+++ src/routes/services/[server]/[...show]/+server.ts
@@ -1,9 +1,13 @@
-import { getServerByKeyOrFail } from '$lib/scraper';
-import { error, json, type RequestHandler } from '@sveltejs/kit';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { getServerByKeyOrFail } from '$lib/scraper'
+import { error, json, type RequestHandler } from '@sveltejs/kit'
 export const GET: RequestHandler = async ({ params, url }) => {
 	const server = getServerByKeyOrFail(params.server as string);
 
-	const show = await server.show(params.show as string);
+	const target = new URL(params.show as string)
+	target.search = url.search
+
+	const show = await server.show(target.toString());
 	if (!show) throw error(404, 'Data tidak ditemukan');
 	show.chapters = show.chapters.map((chapter) => {
 		const ref = new URL(url.toString());
--- src/routes/services/[server]/read/[...chapter_link]/+server.ts
+++ src/routes/services/[server]/read/[...chapter_link]/+server.ts
@@ -1,9 +1,12 @@
-import { getServerByKeyOrFail } from '$lib/scraper';
-import { json, type RequestHandler } from '@sveltejs/kit';
+import { getServerByKeyOrFail } from '$lib/scraper'
+import { json, type RequestHandler } from '@sveltejs/kit'
 
 export const GET: RequestHandler = async ({ params, url }) => {
 	const server = getServerByKeyOrFail(params.server as string);
 
-	const resp = await server.read(params.chapter_link as string);
+	const target = new URL(params.chapter_link as string)
+	target.search = url.search
+
+	const resp = await server.read(target.toString());
 	return json(resp);
 };
