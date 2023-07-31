9451e1c27f2042b9b4865db85e98d020e35a7fec
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-07-31 10:58:53 +0700

    bacakomik next

--- src/lib/server/scraper/id/Bacakomik.ts
+++ src/lib/server/scraper/id/Bacakomik.ts
@@ -1,87 +1,111 @@
-import { refererImage } from '$lib/mirrorimage';
-import BaseKomik, { type Chapter, type Komik, type KomikDetail } from '../BaseKomik';
-import type { ReadChapter } from '../BaseKomik/interfaces';
+import { refererImage } from '$lib/mirrorimage'
+import BaseKomik, { type Chapter, type Komik, type KomikDetail } from '../BaseKomik'
+import type { ReadChapter } from '../BaseKomik/interfaces'
 
 class Bacakomik extends BaseKomik {
 	website = 'https://bacakomik.co/';
 	logo = 'https://i0.wp.com/bacakomik.co/wp-content/uploads/2020/09/bacakomikv2.png';
 	lang = 'indonesia';
-	async list(searchParams: URLSearchParams): Promise<Komik[]> {
-		const keyword = searchParams.get('q') || '';
-		searchParams.delete('q');
-		const link = new URL('https://bacakomik.co/?s=');
-		searchParams.set('s', keyword);
-		link.search = searchParams.toString();
 
-		const $ = await this.requestCheerio(link.toString());
-		const results: Komik[] = [];
+	async nextPage(next: number) {
+
+		const link = new URL(this.website)
+		link.pathname = '/komik-terbaru/page/' + next
+
+		const $ = await this.requestCheerio(link.toString())
+		const results: Komik[] = []
 		$('.animepost').each((i, el) => {
-			const anchorAttribute = $(el).find('a').attr()!;
-			const img = $(el).find('img').attr()!['src'];
+			const anchorAttribute = $(el).find('a').attr()!
+			const img = $(el).find('img').attr()!['src']
 			results.push({
 				show: anchorAttribute['href'],
 				title: anchorAttribute['title'].replace(/^komik\s+/i, ''),
 				img
-			});
-		});
+			})
+		})
 
-		return results;
+		return results
+	}
+	async list(searchParams: URLSearchParams): Promise<Komik[]> {
+		const next = +(searchParams.get('next')?.toString() || '')
+		if (next) return this.nextPage(next)
+
+
+		const keyword = searchParams.get('q') || ''
+		searchParams.delete('q')
+		const link = new URL(this.website)
+		searchParams.set('s', keyword)
+		link.search = searchParams.toString()
+
+		const $ = await this.requestCheerio(link.toString())
+		const results: Komik[] = await this.nextPage(next || 1)
+		$('.animepost').each((i, el) => {
+			const anchorAttribute = $(el).find('a').attr()!
+			const img = $(el).find('img').attr()!['src']
+			results.push({
+				show: anchorAttribute['href'],
+				title: anchorAttribute['title'].replace(/^komik\s+/i, ''),
+				img
+			})
+		})
+
+		return results
 	}
 
 	async show(link: string): Promise<KomikDetail | null> {
-		const chapFuture = this.chapters(link);
-		const $ = await this.requestCheerio(link);
+		const chapFuture = this.chapters(link)
+		const $ = await this.requestCheerio(link)
 		const title = $('.infoanime h1.entry-title')
 			.text()
-			.replace(/^komik\s+/i, '');
-		const img = $('.infoanime .thumb img').attr()!['src'];
+			.replace(/^komik\s+/i, '')
+		const img = $('.infoanime .thumb img').attr()!['src']
 
-		const chapters: Chapter[] = await chapFuture;
-		return { title, img, chapters };
+		const chapters: Chapter[] = await chapFuture
+		return { title, img, chapters }
 	}
 
 	async chapters(link: string): Promise<Chapter[]> {
-		const $ = await this.requestCheerio(link);
-		const chapters: Chapter[] = [];
+		const $ = await this.requestCheerio(link)
+		const chapters: Chapter[] = []
 		$('#chapter_list li').each((i, el) => {
 			chapters.push({
 				link: $(el).find('a chapter').parent().attr()!['href'],
 				title: `Chapter ${$(el).find('a chapter').text()}`
-			});
-		});
-		return chapters;
+			})
+		})
+		return chapters
 	}
 
 	async read(chapter_link: string): Promise<ReadChapter | null> {
-		const $ = await this.requestCheerio(chapter_link);
+		const $ = await this.requestCheerio(chapter_link)
 		const title = $('.chapter-content h1.entry-title')
 			.text()
-			.replace(/^komik\s+/i, '');
+			.replace(/^komik\s+/i, '')
 
-		const prevAttr = $('.nextprev:first-child a:first-child[rel="prev"]').attr();
-		const prev = prevAttr ? prevAttr['href'] : null;
-		const nextAttr = $('.nextprev:first-child a:last-child[rel="next"]').attr();
-		const next = nextAttr ? nextAttr['href'] : null;
-		const showLink = $('.ls1 a').attr()!['href'];
+		const prevAttr = $('.nextprev:first-child a:first-child[rel="prev"]').attr()
+		const prev = prevAttr ? prevAttr['href'] : null
+		const nextAttr = $('.nextprev:first-child a:last-child[rel="next"]').attr()
+		const next = nextAttr ? nextAttr['href'] : null
+		const showLink = $('.ls1 a').attr()!['href']
 
-		const chapterImages: string[] = [];
+		const chapterImages: string[] = []
 		$('#chimg-auh img').each((i, el) => {
-			const onErrorAttr = $(el).attr()!.onerror.toString();
-			let image = $(el).attr()!['src'];
+			const onErrorAttr = $(el).attr()!.onerror.toString()
+			let image = $(el).attr()!['src']
 			if (onErrorAttr.includes('this.src')) {
-				image = onErrorAttr.replace(/^this\.onerror=null;this\.src='/i, '').replace(/';$/i, '');
+				image = onErrorAttr.replace(/^this\.onerror=null;this\.src='/i, '').replace(/';$/i, '')
 			}
 
-			chapterImages.push(refererImage(image, chapter_link));
-		});
+			chapterImages.push(refererImage(image, chapter_link))
+		})
 		return {
 			title,
 			next,
 			prev,
 			chapterImages,
 			showLink
-		};
+		}
 	}
 }
 
-export default Bacakomik.getInstance();
+export default Bacakomik.getInstance()
