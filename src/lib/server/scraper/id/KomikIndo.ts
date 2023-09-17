97d6c00dd70c6b6b4d9f794da6de91dda99e2486
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-09-17 10:53:23 +0700

    fix: komikindo server

--- src/lib/server/scraper/id/KomikIndo.ts
+++ src/lib/server/scraper/id/KomikIndo.ts
@@ -3,85 +3,85 @@ import BaseKomik, { type Chapter, type Komik, type KomikDetail } from '../BaseKo
 import type { ReadChapter } from '../BaseKomik/interfaces'
 
 class KomikIndo extends BaseKomik {
-	website = 'https://komikindo.one';
+	website = 'https://komikindo.tv';
 	logo = 'https://cdn.kena-blok.xyz/uploads/2020/12/komikindo.png?width=140';
 	lang = 'indonesia';
 	async list(searchParams: URLSearchParams): Promise<Komik[]> {
-		const link = new URL(this.website);
-		searchParams.set('s', searchParams.get('q') || '');
-		searchParams.delete('q');
-		link.search = searchParams.toString();
+		const link = new URL(this.website)
+		searchParams.set('s', searchParams.get('q') || '')
+		searchParams.delete('q')
+		link.search = searchParams.toString()
 
-		const $ = await this.requestCheerioHumanoid(link.toString());
-		const results: Komik[] = [];
+		const $ = await this.requestCheerioHumanoid(link.toString())
+		const results: Komik[] = []
 		$('.animepost').each((i, el) => {
-			const anchorAttribute = $(el).find('a').attr()!;
-			const img = $(el).find('img').attr()!['src'];
+			const anchorAttribute = $(el).find('a').attr()!
+			const img = refererImage($(el).find('img').attr()!['src'], link.toString())
 			results.push({
 				show: anchorAttribute['href'],
 				title: anchorAttribute['title'].replace(/^komik\s+/i, ''),
 				img
-			});
-		});
+			})
+		})
 
-		return results;
+		return results
 	}
 
 	async show(link: string): Promise<KomikDetail | null> {
-		const chapFuture = this.chapters(link);
-		const $ = await this.requestCheerioHumanoid(link);
+		const chapFuture = this.chapters(link)
+		const $ = await this.requestCheerioHumanoid(link)
 		const title = $('.infoanime h1.entry-title')
 			.text()
-			.replace(/^komik\s+/i, '');
-		const img = $('.infoanime .thumb img').attr()!['src'];
+			.replace(/^komik\s+/i, '')
+		const img = refererImage($('.infoanime .thumb img').attr()!['src'], link)
 
-		const chapters: Chapter[] = await chapFuture;
-		return { title, img, chapters };
+		const chapters: Chapter[] = await chapFuture
+		return { title, img, chapters }
 	}
 
 	async chapters(link: string): Promise<Chapter[]> {
-		const $ = await this.requestCheerioHumanoid(link);
-		const chapters: Chapter[] = [];
+		const $ = await this.requestCheerioHumanoid(link)
+		const chapters: Chapter[] = []
 		$('#chapter_list li').each((i, el) => {
 			chapters.push({
 				link: $(el).find('a chapter').parent().attr()!['href'],
 				title: `Chapter ${$(el).find('a chapter').text()}`
-			});
-		});
-		return chapters;
+			})
+		})
+		return chapters
 	}
 
 	async read(chapter_link: string): Promise<ReadChapter | null> {
-		const $ = await this.requestCheerioHumanoid(chapter_link);
+		const $ = await this.requestCheerioHumanoid(chapter_link)
 		const title = $('.chapter-content h1.entry-title')
 			.text()
-			.replace(/^komik\s+/i, '');
+			.replace(/^komik\s+/i, '')
 
-		const prevAttr = $('.nextprev:first-child a:first-child[rel="prev"]').attr();
-		const prev = prevAttr ? prevAttr['href'] : null;
-		const nextAttr = $('.nextprev:first-child a:last-child[rel="next"]').attr();
-		const next = nextAttr ? nextAttr['href'] : null;
-		const showLink = $('.nextprev:first-child a .daftarch').parent().attr()!['href'];
+		const prevAttr = $('.nextprev:first-child a:first-child[rel="prev"]').attr()
+		const prev = prevAttr ? prevAttr['href'] : null
+		const nextAttr = $('.nextprev:first-child a:last-child[rel="next"]').attr()
+		const next = nextAttr ? nextAttr['href'] : null
+		const showLink = $('.nextprev:first-child a .daftarch').parent().attr()!['href']
 
-		const chapterImages: string[] = [];
+		const chapterImages: string[] = []
 
 		$('#chimg-auh img').each((i, el) => {
-			const onErrorAttr = $(el).attr()!.onerror.toString();
-			let image = $(el).attr()!['src'];
+			const onErrorAttr = $(el).attr()!.onerror.toString()
+			let image = $(el).attr()!['src']
 			if (onErrorAttr.includes('this.src')) {
-				image = onErrorAttr.replace(/^this\.onerror=null;this\.src='/i, '').replace(/';$/i, '');
+				image = onErrorAttr.replace(/^this\.onerror=null;this\.src='/i, '').replace(/';$/i, '')
 			}
 
-			chapterImages.push(refererImage(image, chapter_link));
-		});
+			chapterImages.push(refererImage(image, chapter_link))
+		})
 		return {
 			title,
 			next,
 			prev,
 			chapterImages,
 			showLink
-		};
+		}
 	}
 }
 
-export default KomikIndo.getInstance();
+export default KomikIndo.getInstance()
